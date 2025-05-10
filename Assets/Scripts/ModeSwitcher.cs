72c3014159d83a689b9b0751f6f62dcee662056c
LuQmu5 <luqmu5.1007@gmail.com>
2025-05-10 17:26:29 +0400

    кайф

---
+++ Assets/Scripts/ModeSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ModeSwitcher : MonoBehaviour
+{
+    [SerializeField] private Animator[] animators;
+    [SerializeField] private string destroyerAnimationName = "Destroyer";
+    [SerializeField] private string destroyerOffAnimationName = "DestroyerOff";
+
+    private bool isCollecting = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SwitchMode();
+        }
+    }
+
+    private void SwitchMode()
+    {
+        isCollecting = !isCollecting;
+
+        foreach (var animator in animators)
+        {
+            if (animator == null) continue;
+
+            string animationToPlay = isCollecting ? destroyerOffAnimationName : destroyerAnimationName;
+            animator.speed = 1f;
+            animator.Play(animationToPlay, 0, 0f);
+        }
+    }
+}
